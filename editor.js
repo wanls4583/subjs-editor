e0a34698a0dcec978609da4a6951f2be2bd2caa7
wanls4583 <409051987@qq.com>
2018-06-29 22:56:07 +0800

    add file

---
+++ editor.js
@@ -0,0 +1,100 @@
+! function() {
+	var Util = {
+		insertStr: function(str,index,cont){
+			return str.substring(0,index) + cont + str.substr(index);
+		}
+	}
+
+    function SubJs() {
+        this.currentNode = null; //光标所在的节点
+        this.cureentLine = 1;
+        this.currentContent = ''; //光标所在的内容区域 
+        this.lines = []; //所有的行
+    }
+
+    var _proto = SubJs.prototype;
+
+    _proto.hightlight = function(str) {
+        var reg = [
+            /^[^'"]*\/\*[\s\S]*?(?:\*\/)?/mg, //多行注释
+            /^[^'"]*\/\/[\s\S]*/g, //单行注释
+            /'[\s\S]*?'|"[\s\S]*?"/g, //字符串
+            /\b(?:break|continue|do|else|for|if|return|while|var|function|new|class)\b/g, //关键字
+            /\!==|\!=|==|=|\?|\&\&|\&|\|\||\||>=|>|<=|<|\+=|\+|\-=|\-|\*=|\*|\/=|\//g, //操作符
+            /\d+|[\b^](?:undefined|null)[\b&]/g, //数字
+            /[.]?([\w]+)(?=\()/g, //方法名
+        ]
+
+        var lineObj = {};
+        var doneArr = []; //已处理过的区域
+
+        for (var i = 0; i < reg.length; i++) {
+            var match = null;
+            var regObj = reg[i];
+            while (match = regObj.exec(str)) {
+                var start, end;
+                if(!match[1]){
+                	start = match.index;
+                	end = start + match[0].length - 1;
+                }else{
+                	start = match.index + match[0].indexOf(match[1]);
+                	end = start + match[1].length - 1;
+                }
+                var className = '';
+                var ifDo = true;
+                for (var tmp = 0; tmp < doneArr.length; tmp++) {
+                    if (start >= doneArr[tmp].start && start <= doneArr[tmp].end ||
+                        end >= doneArr[tmp].start && end <= doneArr[tmp].end) {
+                        ifDo = false;
+                    }
+                }
+                if (!ifDo) {
+                    continue;
+                }
+
+                switch (i) {
+                    case 0:
+                        className = 'multi_comment';
+                        break;
+                    case 1:
+                        className = 'comment';
+                        break;
+                    case 2:
+                        className = 'string';
+                        break;
+                    case 3:
+                        className = 'key';
+                        break;
+                    case 4:
+                        className = 'oprator';
+                        break;
+                    case 5:
+                        className = 'number';
+                        break;
+                    case 6:
+                        className = 'method';
+                        break;
+                }
+                doneArr.push({ start: start, end: end, className: className });
+            }
+            regObj.lastIndex = 0;
+        }
+        doneArr.sort(function(arg1, arg2) {
+            if (arg1.start < arg2.start) {
+                return -1
+            } else if (arg1.start == arg2.start) {
+                return 0;
+            } else {
+                return 1;
+            }
+        })
+        for (var i = doneArr.length - 1; i >= 0; i--) {
+        	var obj = doneArr[i];
+        	str = Util.insertStr(str,obj.end+1,'</span>');
+        	str = Util.insertStr(str,obj.start,'<span class="'+obj.className+'">');
+        }
+        console.log(str);
+        return str;
+    }
+    window.SubJs = SubJs;
+}()
