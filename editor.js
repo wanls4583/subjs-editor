5bd52f29510511bf4854b215ce9ba5c53a38c1e2
wanls4583 <409051987@qq.com>
2018-07-02 16:25:51 +0800

    codding

--- editor.js
+++ editor.js
@@ -67,11 +67,13 @@
 
     _proto._init = function() {
         let self = this;
-        this.$context = $('<div class="editor_context" style="position:relative;z-index:1;min-height:100%;margin:0 15px 0 5px;"></div>');
-        this.$leftNumBg = $('<div class="line_num_bg" style="position:absolute;top:0;left:0;z-index:0;width:40px;height:100%"></div>')
-        this.$wrapper.append(this.$context);
+        var $wrap = $('<div class="editor_wrap" style="height:100%;overflow:auto;padding:5px 0 0 40px,position:relative;z-index:1"></div>');
+        this.$leftNumBg = $('<div class="line_num_bg" style="position:absolute;top:0;left:0;width:40px;height:100%"></div>');
+        this.$context = $('<div class="editor_context" style="position:relative;z-index:1;min-height:100%;margin:0 15px 0 45px;"></div>');
+        $wrap.append(this.$context);
         this.$wrapper.append(this.$leftNumBg);
-        this.$wrapper.css({ padding: '5px 0 0 40px', position: 'relative' });
+        this.$wrapper.append($wrap);
+        this.$wrapper.css({position:'relative'});
         //全角符号和中文字符
         this.fullAngleReg = /[\x00-\x1f\x80-\xa0\xad\u1680\u180E\u2000-\u200f\u2028\u2029\u202F\u205F\u3000\uFEFF\uFFF9-\uFFFC]|[\u1100-\u115F\u11A3-\u11A7\u11FA-\u11FF\u2329-\u232A\u2E80-\u2E99\u2E9B-\u2EF3\u2F00-\u2FD5\u2FF0-\u2FFB\u3000-\u303E\u3041-\u3096\u3099-\u30FF\u3105-\u312D\u3131-\u318E\u3190-\u31BA\u31C0-\u31E3\u31F0-\u321E\u3220-\u3247\u3250-\u32FE\u3300-\u4DBF\u4E00-\uA48C\uA490-\uA4C6\uA960-\uA97C\uAC00-\uD7A3\uD7B0-\uD7C6\uD7CB-\uD7FB\uF900-\uFAFF\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE66\uFE68-\uFE6B\uFF01-\uFF60\uFFE0-\uFFE6]|[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
         this.getCharWidth();
