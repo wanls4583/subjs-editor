108c23c4272697df9123ed80701dc762b0fdc75b
wanls4583 <409051987@qq.com>
2018-08-02 11:08:59 +0800

    codding

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ var path = require('path');
 
 // 拼接我们的工作区路径为一个绝对路径
 function resolve(dir) {
-    return path.join(__dirname, '..', dir);
+    return path.join(__dirname, dir);
 }
 
 module.exports = {
-    entry: resolve('test.js'),
+    entry: './src/test.js',
     output: {
         // 编译输出的根路径
-        path: 'dist',
+        path:  resolve('dist'),
         // 编译输出的文件名
         filename: 'bundle.js',
         // 正式发布环境下编译输出的发布路径
-        publicPath: './'
+        // publicPath: './'
     },
     resolve: {
         // 自动补全的扩展名
@@ -28,20 +28,9 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.js/,
-            loader: 'eslint-loader',
-            enforce: "pre",
-            include: [resolve('src')],
-            options: {
-                formatter: require('eslint-friendly-formatter')
-            }
-        }, {
             test: /\.js$/,
             loader: 'babel-loader',
             include: [resolve('src'), resolve('test')]
-        }, {
-            test: require.resolve('zepto'),
-            loader: 'exports-loader?window.Zepto!script-loader'
         }]
     }
 }
